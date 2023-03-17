cddc8470f4b28c8d03835c95c51166bd8b07a9f6
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2023-03-17 21:28:55 +0100

    pizzas4

--- models/pizzas.js
+++ models/pizzas.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 
 const pizzasSchema = new mongoose.Schema(
     {
-        name: { type: String, unique: true},
-        mass: { type: [String], enum: ["fina", "normal"] },
-        size: { type: [String], enum: ["pequeña", "mediana", "familiar"]},
-        dip: {type: [String], enum: ["barbacoa", "carbonara", "tomate", "napolitana"]},
-        ingredients:  { type: String},
-        price: { type: Number, required: true},
-        account: {type: Number},
-        picture: String,
+        name: String
     },
     {
         timestamps: true
