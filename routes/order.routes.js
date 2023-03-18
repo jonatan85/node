5439f80c332812b618ec152a1750ec1e577d1612
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2023-03-18 12:18:50 +0100

    feat(jonatan-moreno)order6

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -34,64 +34,20 @@ orderRouter.get('/', async(req, res) => {
     }
   });
  
- orderRouter.post('/order', async (req, res, next) => {
-     try {
-        const newOrder = new Order({
-           ...req.body.items,
-           ...req.body.total 
-        });
-        const createOrder = await newOrder.save();
-         
-        return res.status(201).json(createOrder);
-     } catch(err) {
-        next(err);
-     }
-  });
- 
- orderRouter.put('/add-pizza', async (req, res, next) => {
+  orderRouter.post('/order', async (req, res, next) => {
     try {
-        const {orderId, pizzaId} = req.body;
-        if(!orderId) {
-            return next(createError('Se necesita un id de orden para poder añadir una pizza', 500))
-        }
-        if(!pizzaId) {
-            return next(createError('Se necesita un id de pizza para añadir a la orden', 500))
-        }
-        const addOrder = await Order.findByIdAndUpdate(
-            orderId,
-            {$push: {pizza: pizzaId}},
-            {new: true }
-        );
-        return res.status(200).json(addOrder);
-    } catch(err) {
-        next(err);
-    }
- });
- 
- orderRouter.put('/:id', async (req, res, next) => {
-    try {
-       const id = req.params.id;
-       const modifiedOrder = new Order({...req.body});
-       modifiedOrder._id = id;
-       const orderUpdate = await Order.findByIdAndUpdate(
-          id,
-          modifiedOrder,
-          {new: true}
-       );
-       return res.status(200).json(orderUpdate);
-    }catch (err) {
-       next(err);
+      const newOrder = new Order({
+        items: req.body.items.map(item => Object.assign({}, item)),
+        total: req.body.total
+      });
+      const createOrder = await newOrder.save();
+      return res.status(201).json(createOrder);
+    } catch (err) {
+      next(err);
     }
   });
  
- orderRouter.delete('/:id', async (req, res, next) => {
-    try{  
-       const id = req.params.id;
-       await Order.findByIdAndDelete(id);
-       return res.status(200).json('La orden se elimino correctamente.')
-    } catch(err) {
-       next(err);
-    }
-  });
+ 
+
  
  module.exports = orderRouter;
